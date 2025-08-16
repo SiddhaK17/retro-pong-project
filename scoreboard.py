cfe3d0db0f8439ae7d965d6c1d8d52add4af87ce
SiddhaK17 <siddhakad17@gmail.com>
2025-08-16 21:53:23 +0530

    feat: implement Scoreboard class for tracking points
    - Added a Scoreboard class to display and manage scoring.
    - Implemented methods to increment player and opponent scores dynamically.
    - Designed the scoreboard to update in real time within the main game loop.
    - Used modular structure to keep score logic independent from ball and paddle mechanics.
    - Enhanced the user experience with clear and visually aligned score tracking.
    - Provides a scalable structure for adding game ending conditions (e.g., first to 10 points).

---
+++ scoreboard.py
@@ -0,0 +1,32 @@
+import os
+os.environ['TCL_LIBRARY'] = r'C:\Program Files\Python313\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Program Files\Python313\tcl\tk8.6'
+
+from turtle import Turtle, Screen
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))  # here the normal is the font weight and 80 is points for the font to be
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))  # here the normal is the font weight and 80 is points for the font to be
+
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
